461bc7437c0a7f16545d61997f05f1d77803e82f
MineStar <band1019@naver.com>
2022-07-21 21:31:57 +0900

    #

--- lib/data/operator/DefenseOperator.dart
+++ lib/data/operator/DefenseOperator.dart
@@ -2,42 +2,44 @@
 
 import 'package:r6splannerboard/data/operator/Operator.dart';
 
+import '../gadget/Gadget.dart';
+
 enum DefenseOperator implements Operator {
-  SMOKE("스모크"),
-  MUTE("뮤트"),
-  CASTLE("캐슬"),
-  PULSE("펄스"),
-  DOC("닥"),
-  ROOK("룩"),
-  KAPKAN("캅칸"),
-  TACHANKA("타찬카"),
-  JAGER("예거"),
-  BANDIT("밴딧"),
-  FROST("프로스트"),
-  VALKYRIE("발키리"),
-  CAVEIRA("카베이라"),
-  ECHO("에코"),
-  MIRA("미라"),
-  LESION("리전"),
-  ELA("엘라"),
-  VIGIL("비질"),
-  MAESTRO("마에스트로"),
-  ALIBI("알리바이"),
-  CLASH("클래쉬"),
-  KAID("카이드"),
-  MOZZIE("모지"),
-  WARDEN("워든"),
-  GOYO("고요"),
-  WAMAI("와마이"),
-  ORYX("오릭스"),
-  MELUSI("멜루시"),
-  ARUNI("아루니"),
-  THUNDERBIRD("썬더버드"),
-  THORN("쏜"),
-  AZAMI("아자미"),
+  SMOKE("스모크", {}),
+  MUTE("뮤트", {}),
+  CASTLE("캐슬", {}),
+  PULSE("펄스", {}),
+  DOC("닥", {}),
+  ROOK("룩", {}),
+  KAPKAN("캅칸", {}),
+  TACHANKA("타찬카", {}),
+  JAGER("예거", {}),
+  BANDIT("밴딧", {}),
+  FROST("프로스트", {}),
+  VALKYRIE("발키리", {}),
+  CAVEIRA("카베이라", {}),
+  ECHO("에코", {}),
+  MIRA("미라", {}),
+  LESION("리전", {}),
+  ELA("엘라", {}),
+  VIGIL("비질", {}),
+  MAESTRO("마에스트로", {}),
+  ALIBI("알리바이", {}),
+  CLASH("클래쉬", {}),
+  KAID("카이드", {}),
+  MOZZIE("모지", {}),
+  WARDEN("워든", {}),
+  GOYO("고요", {}),
+  WAMAI("와마이", {}),
+  ORYX("오릭스", {}),
+  MELUSI("멜루시", {}),
+  ARUNI("아루니", {}),
+  THUNDERBIRD("썬더버드", {}),
+  THORN("쏜", {}),
+  AZAMI("아자미", {}),
   ;
 
-  const DefenseOperator(this.korean);
+  const DefenseOperator(this.korean, this.gadgets);
 
   @override
   final String korean;
@@ -50,4 +52,7 @@ enum DefenseOperator implements Operator {
 
   @override
   String get path => "assets/operator/${teamType.name}/$enumName.png";
+
+  @override
+  final Set<Gadget> gadgets;
 }
